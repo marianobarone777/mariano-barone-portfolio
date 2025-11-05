6eb014c291322f4fdb84156dfc20e3a878ff329a
marianobarone777 <marianobarone@gmail.com>
2025-11-05 16:21:23 -0300

    Configurar deploy a GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Si tu repo se llama p.ej. "mariano-portfolio", usá esa carpeta:
+  base: '/mariano-portfolio/'
 })
+
